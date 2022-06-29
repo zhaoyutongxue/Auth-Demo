4c7f0128034fb8ca2371fc38ccdad52958ca3b72
Zhaoyu(Henry) Zhong <zhaoyuzhong2015@gmail.com>
2022-06-29 22:41:25 +1000

    508. finish auth demo

--- index.js
+++ index.js
@@ -26,31 +26,20 @@ db.once("open", () => {
     console.log("database connected")
 })
 
-// bcrypt async method:
-const hashpassword = async (pw) => {
-    const salt = await bcrypt.genSalt(saltRounds)
-    const hash = await bcrypt.hash(pw, salt);
-    console.log(salt)
-    console.log(hash)
-}
-const login = async (pw, hashedPw) => {
-    const result = await bcrypt.compare(pw, hashedPw);
-    if (result) {
-        console.log('success match! Logged you in! ')
-    } else {
-        console.log("incorrect!")
-    }
-}
 
+
+const requireLogin = (req, res, next) => {
+    if (!req.session.user_id) {
+        return res.redirect('/login')
+    }
+    next()
+}
 
 app.get('/register', (req, res) => {
     res.render('register.ejs')
 })
 
 app.post('/register', async (req, res) => {
-    const salt = await bcrypt.genSalt(10)
-    const hash = await bcrypt.hash(req.body.password, salt)
-    req.body.password = hash;
     const user = new User(req.body)
     await user.save();
     req.session.user_id = user._id;
@@ -61,32 +50,33 @@ app.get('/login', (req, res) => {
     res.render('login.ejs')
 })
 
+
 app.get('/', (req, res) => {
     res.send('Home Page')
 })
 
 app.post('/login', async (req, res) => {
     const { username, password } = req.body
-    const user = await User.findOne({ username: username })
-    const result = await bcrypt.compare(password, user.password)
-    if (result) {
-        req.session.user_id = user._id;
-        req.session.destroy();
+    const foundUser = await User.findAndValidate(username, password)
+    if (foundUser) {
+        req.session.user_id = foundUser._id;
         res.redirect('/secret')
     } else {
         res.redirect('/login')
     }
 })
 
-app.get('/secret', (req, res) => {
-    if (!req.session.user_id) {
-        res.redirect('/login')
-    }
+app.get('/secret', requireLogin, (req, res) => {
     res.render('secret.ejs')
 })
+app.get('/topsecret', requireLogin, (req, res) => {
+    res.send('top secret!')
+})
+
 
 app.post('/logout', (req, res) => {
     req.session.user_id = null;
+    req.session.destroy();
     res.redirect('/login')
 })
 
--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 // this is the schema to save username and the hashed password in the database. 
-
+const bcrypt = require('bcrypt')
 const { string } = require('joi')
 const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
@@ -11,6 +11,21 @@ const userSchema = new mongoose.Schema({
         type: String,
         require: [true, 'password is required!']
     }
+})
+
+userSchema.statics.findAndValidate = async function (username, password) {
+    const foundUser = await this.findOne({ username })
+    const isValid = await bcrypt.compare(password, foundUser.password)
+    return isValid ? foundUser : false;
+}
+
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(this.password, salt)
+    this.password = hash;
+    next()
 })
 
 const User = mongoose.model('User', userSchema)
